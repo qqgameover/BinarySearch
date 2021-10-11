fc8b8e6f2983c8115feec121536318dfa90e0038
qqgameover <kasper35@live.no>
2021-10-11 19:48:03 +0200

    Update Program.cs

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -6,7 +6,33 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numArray = new[] {1, 9, 3, 11, 1099, 14, 12, 11, 35, 10, 19, 183};
+            Array.Sort(numArray);
+            Console.WriteLine(BinarySearchDisplay(numArray, 1));
+        }
+        public static object BinarySearchDisplay(int[] arr, int key)
+        {
+
+            int minNum = 0;
+            int maxNum = arr.Length - 1;
+
+            while (minNum <= maxNum)
+            {
+                int mid = (minNum + maxNum) / 2;
+                if (key == arr[mid])
+                {
+                    return mid;
+                } 
+                if (key < arr[mid])
+                {
+                    maxNum = mid - 1;
+                }
+                else
+                {
+                    minNum = mid + 1;
+                }
+            }
+            return "None";
         }
     }
 }
